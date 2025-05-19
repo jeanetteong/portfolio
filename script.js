2f63a01b00f00848b8e37875a6c6a2364da05d11
jeanetteong <jeanetteongjx@hotmail.com>
2025-05-19 19:21:24 +0800

    19/05/25 fun facts modal

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".navbar-right a");
 
+const open_ctss = document.getElementById('open-ctss');
+const open_sp = document.getElementById('open-sp');
+const open_accenture = document.getElementById('open-accenture');
+
+const modal_ctss = document.getElementById('modal-ctss');
+const modal_sp = document.getElementById('modal-sp');
+const modal_accenture = document.getElementById('modal-accenture');
+
+const close_ctss = document.getElementById('close-ctss');
+const close_sp = document.getElementById('close-sp');
+const close_accenture = document.getElementById('close-accenture');
+
+//navbar scroll spy
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
@@ -21,4 +34,48 @@ const observer = new IntersectionObserver(
 
 sections.forEach((section) => {
     observer.observe(section);
+});
+
+//modal
+//click content box to open
+open_ctss.addEventListener('click', () => {
+    modal_ctss.classList.add('show');
+})
+
+open_sp.addEventListener('click', () => {
+    modal_sp.classList.add('show');
+})
+
+open_accenture.addEventListener('click', () => {
+    modal_accenture.classList.add('show');
+})
+
+//click button to close
+close_ctss.addEventListener('click', () => {
+    modal_ctss.classList.remove('show');
+})
+
+close_sp.addEventListener('click', () => {
+    modal_sp.classList.remove('show');
+})
+
+close_accenture.addEventListener('click', () => {
+    modal_accenture.classList.remove('show');
+})
+
+//click outside to close
+modal_ctss.addEventListener('click', (e) => {
+  if (e.target === modal_ctss) {
+    modal_ctss.classList.remove('show');
+  }
+});
+modal_sp.addEventListener('click', (e) => {
+  if (e.target === modal_sp) {
+    modal_sp.classList.remove('show');
+  }
+});
+modal_accenture.addEventListener('click', (e) => {
+  if (e.target === modal_accenture) {
+    modal_accenture.classList.remove('show');
+  }
 });
